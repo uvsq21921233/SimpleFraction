ab7c294096662ac5e4085e68e395317cb8a60c86
uvsq21921233 <boressan.ye@ens.uvsq.fr>
2020-10-04 19:10:05 +0200

    Commit final

--- SimpleFraction/main.java
+++ SimpleFraction/main.java
@@ -1,10 +1,9 @@
 package SimpleFraction;
-public class main {
 
-    public static void main(String[] args){
-        Fraction F= new Fraction(2,3);
-        String s=F.toString();
-        System.out.println(s); 
+public class main {
+    public static void main(String[] args) {
+        Fraction F = new Fraction(2, 3);
+        System.out.println(F.toString());
     }
-   
+
 }
